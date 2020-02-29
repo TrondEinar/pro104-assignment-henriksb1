4dfcfe434a4a0702bb9b36b732651e5a49b9c553
Brage Svensli <brage.svensli@gmail.com>
2020-02-29 17:30:39 +0100

    Dette er en test av committ fra Visual Studio

--- docs/main.js
+++ docs/main.js
@@ -15,12 +15,7 @@ var input_task_name_element = document.querySelector("[name='input_task_name']")
 var input_task_description_element = document.querySelector("[name='input_task_description']");
 var input_task_submit_element = document.getElementById('input_task_submit');
 
-// Set onClick()
-
-input_user_submit_element.onclick = input_user_submit_clicked;
-input_task_submit_element.onclick = input_task_submit_clicked;
-
-// Array
+// Functions
 
 function input_user_submit_clicked() { // Funksjon for å legge til ny "user"
     current_users = JSON.parse(window.localStorage.getItem('users')) || []; // Henter alle de lagrede brukerene og legger de inn i et array(current_users)
@@ -48,5 +43,11 @@ function update_view() { // Funksjon for å oppdatere det som syntes på siden.
     console.log("Users: " + users_JSON);
     console.log("Tasks: " + tasks_JSON);
 }
+
+
+// Set onClick()
+
+input_user_submit_element.onclick = input_user_submit_clicked;
+input_task_submit_element.onclick = input_task_submit_clicked;
 
 update_view(); // Run on page load
